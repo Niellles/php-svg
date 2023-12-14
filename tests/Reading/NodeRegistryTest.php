e82eb72187cfe6ae4bb9feadcc7ba249cc726fa2
Niellles <10075507+Niellles@users.noreply.github.com>
2023-12-14 20:51:56 +0100

    test: Cover all node types in NodeRegistry
    Add tests for all known node types that can be created by NodeRegistry.

--- tests/Reading/NodeRegistryTest.php
+++ tests/Reading/NodeRegistryTest.php
@@ -2,8 +2,10 @@
 
 namespace SVG\Reading;
 
+use ReflectionProperty;
 use SVG\Nodes\Shapes\SVGRect;
 use SVG\Nodes\SVGGenericNodeType;
+use SVG\Nodes\SVGNode;
 
 /**
  * @covers \SVG\Reading\NodeRegistry
@@ -16,6 +18,25 @@ class NodeRegistryTest extends \PHPUnit\Framework\TestCase
     {
         $result = NodeRegistry::create('rect');
         $this->assertInstanceOf(SVGRect::class, $result);
+    }
+
+    /** @dataProvider provideItConstructsAllKnownTypes */
+    public function testItConstructsAllKnownTypes($type,$expectedResult)
+    {
+        $result = NodeRegistry::create($type);
+        $this->assertInstanceOf($expectedResult, $result);
+        $this->assertInstanceOf(SVGNode::class, $result);
+    }
+
+    public function provideItConstructsAllKnownTypes()
+    {
+        $reflectedProperty = new ReflectionProperty(NodeRegistry::class, 'nodeTypes');
+        $reflectedProperty->setAccessible(true);
+        $types = $reflectedProperty->getValue();
+
+        foreach ($types as $type => $class) {
+            yield "\$type='$type'" => [$type, $class];
+        }
     }
 
     public function testShouldUseGenericTypeForOthers()

6e890ca36d7f57135290bb68d2a32611845c36e7
Niellles <10075507+Niellles@users.noreply.github.com>
2024-02-09 12:04:58 +0100

    in progress

--- tests/Nodes/NodeTypeTest.php
+++ tests/Nodes/NodeTypeTest.php
@@ -2,15 +2,41 @@
 
 namespace SVG\Nodes;
 
+use Illuminate\Support\Str;
 use PHPUnit\Framework\TestCase;
 use SVG\Nodes\Shapes\SVGRect;
 use ValueError;
 
 class NodeTypeTest extends TestCase
 {
-    public function testCases()
+    /** @dataProvider provideCases */
+    public function testCases($nodeTypeFQN)
     {
-        $this->assertIsArray(NodeType::cases());
+        $SVGNodeFqn = SVGNode::class;
+        $this->assertTrue(is_a($nodeTypeFQN, $SVGNodeFqn, true), "$nodeTypeFQN is not a $SVGNodeFqn");
+    }
+
+    /** @provides testCases */
+    public function provideCases()
+    {
+        foreach (NodeType::cases() as $nodeType => $fqn) {
+            yield $nodeType => [$fqn];
+        }
+    }
+
+    /** @dataProvider  provideEnforceNodeTypeNamingConvention */
+    public function testEnforceNodeTypeNamingConvention($nodeType, $nodeFQN)
+    {
+        $this->markTestSkipped("Should be completed after confirmation this is desirable");
+        [$result, $message] = $this->checkNamingConvention($nodeType, $nodeFQN);
+        $this->assertTrue($result, $message);
+    }
+
+    public function provideEnforceNodeTypeNamingConvention()
+    {
+        foreach (NodeType::cases() as $nodeType => $fqn) {
+            yield $nodeType => [$nodeType, $fqn];
+        }
     }
 
     /** @dataProvider provideTryFrom */
@@ -39,5 +65,38 @@ class NodeTypeTest extends TestCase
     {
         $this->expectException(ValueError::class);
         NodeType::from('FooBar');
+    }
+
+    private function checkNamingConvention(string $nodeType, string $nodeFQN): array
+    {
+        $expectedNodeTypeName = $this->nodeFqnToNodeType($nodeFQN);
+        $result = ($expectedNodeTypeName == $nodeType);
+
+        if ($result === false) {
+            $message = sprintf(
+                'Expected node type "%s" got %s',
+                $expectedNodeTypeName,
+                $nodeType
+            );
+        }
+
+        return [$result, $message ?? ''];
+    }
+
+    private function nodeFqnToNodeType($nodeFQN): string
+    {
+        // In: \Nodes\Embedded\SVGForeignObject
+        // out: feMergeNode
+        $fqnParts = explode('\\', $nodeFQN);
+        $className = end($fqnParts);
+        $expectedNodeType = Str::replaceStart('SVG', '', $className);
+
+        if (Str::startsWith($expectedNodeType, 'FE')) {
+            $expectedNodeType = Str::replaceStart('FE', 'fe', $expectedNodeType);
+        } else {
+            $expectedNodeType = lcfirst($expectedNodeType);
+        }
+
+        return $expectedNodeType;
     }
 }
